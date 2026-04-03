c0f2f26e1657eb453666b570c4861fe3d2174355
ridham.patel <rpate129@asu.edu>
2026-04-03 05:10:54 -0700

    chore: remove C++ build artifacts from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ridhamap/monostructure_alpha_signal/hf-microstructure
-# Build directory: /Users/ridhamap/monostructure_alpha_signal/hf-microstructure/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/ridhamap/monostructure_alpha_signal/hf-microstructure/build/hf_tests-b12d07c_include.cmake")
-subdirs("_deps/catch2-build")
-subdirs("_deps/nanobench-build")
